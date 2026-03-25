e74ba1bd7d8ed1a473a7a7dc5eec236ec9cdc6aa
JeevsLearns <jadhavjeevita47@gmail.com>
2026-03-25 17:24:36 +0530

    Add files via upload

---
+++ count down.py
@@ -0,0 +1,8 @@
+#COUNT DOWN TIMER  *with 1-second gap)
+import time
+count=int(input("enter the counter num:"))
+print("/n countdown start now:")
+for i in range(count,0,-1):
+    print(i)
+    time.sleep(1)
+print("/n  WOHOOO! Happy New Year")   
